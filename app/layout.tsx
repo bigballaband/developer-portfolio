a335373d9d048137cdefcd570f832a08aef95ef8
Thomas Thangarajah <tomthaya5@gmail.com>
2025-03-03 02:33:49 -0500

    add site name

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+const name = 'Thomas Thangarajah\'s Website!';
+export const siteTitle = 'Thomas Thangarajah\'s website';
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
